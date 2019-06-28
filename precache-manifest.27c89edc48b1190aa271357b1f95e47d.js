d72fde5783816e2cde47fa7e2232e1f1abce40f9
github username <github email address>
2019-06-28 15:50:59 -0700

    Updates

---
+++ precache-manifest.27c89edc48b1190aa271357b1f95e47d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60f24486828a5ac73489661c41e4986f",
+    "url": "/tutor_calculator/index.html"
+  },
+  {
+    "revision": "4c94f180566977f3152c",
+    "url": "/tutor_calculator/static/css/main.e468fcab.chunk.css"
+  },
+  {
+    "revision": "e90574d74274c7fb4d49",
+    "url": "/tutor_calculator/static/js/2.c39fed63.chunk.js"
+  },
+  {
+    "revision": "4c94f180566977f3152c",
+    "url": "/tutor_calculator/static/js/main.714b0c2c.chunk.js"
+  },
+  {
+    "revision": "8f74fc91d06a5bab9d59",
+    "url": "/tutor_calculator/static/js/runtime~main.7fd99e5d.js"
+  }
+]);
